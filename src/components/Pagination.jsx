384a4fdb17f80389aa23871a3cc86201867984b5
unknown <fahadzahidd24@gmail.com>
2023-08-15 21:07:27 +0500

    Final fixes

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -3,7 +3,7 @@ import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
 import { useParams, useNavigate } from "react-router-dom";
 
 import Logo from "../assets/google-pagination-logo.png";
-import { pagination } from "../utils/constants";
+import { pagination } from "../utils/Constants";
 
 const Pagination = ({ queries }) => {
     const { query } = useParams();
